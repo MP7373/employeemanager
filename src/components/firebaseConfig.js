cb6d7e464c6fec5c0bcc45d593405b06243e6d40
MP7373 <martinpayne73@gmail.com>
2017-12-19 06:39:55 -0500

    initialize vue with npm and add firebase init and config files

---
+++ src/components/firebaseConfig.js
@@ -0,0 +1,8 @@
+export default {
+  apiKey: 'AIzaSyBaz-TdoxxhmGef7scBtLy-9EcdHawhc58',
+  authDomain: 'vuefirebaseapp-dfad0.firebaseapp.com',
+  databaseURL: 'https://vuefirebaseapp-dfad0.firebaseio.com',
+  projectId: 'vuefirebaseapp-dfad0',
+  storageBucket: 'vuefirebaseapp-dfad0.appspot.com',
+  messagingSenderId: '330177302194'
+}

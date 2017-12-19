2e36e61ef2431be1711468e9d15eb6719fc1f7e7
MP7373 <martinpayne73@gmail.com>
2017-12-19 07:34:58 -0500

    make vue components, add materialize for styling, set up routes

---
+++ src/components/NewEmployee.vue
@@ -0,0 +1,15 @@
+<template>
+  <div id ="NewEmployee">
+    <h3>NewEmployee</h3>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'NewEmployee',
+  data () {
+    return {
+    }
+  }
+}
+</script>

0418d73d1a90b6e7ce8d6b9da39eb45f592f753e
MP7373 <martinpayne73@gmail.com>
2017-12-20 21:56:59 -0500

    add request to firebase to display sample employee data on dashboard

--- src/components/firebaseInit.js
+++ src/components/firebaseInit.js
@@ -1,5 +1,14 @@
 import firebase from 'firebase'
 import 'firebase/firestore'
-import firebaseConfig from './firebaseConfig'
-const firebaseApp = firebase.initializeApp(firebaseConfig)
+
+const config = {
+  apiKey: 'AIzaSyCO15WW1OGZIh6K_g9CdqFcfETS6Em8Gvw',
+  authDomain: 'funprojectyay1234superduperhay.firebaseapp.com',
+  databaseURL: 'https://funprojectyay1234superduperhay.firebaseio.com',
+  projectId: 'funprojectyay1234superduperhay',
+  storageBucket: 'funprojectyay1234superduperhay.appspot.com',
+  messagingSenderId: '1094413138035'
+}
+
+const firebaseApp = firebase.initializeApp(config)
 export default firebaseApp.firestore()
